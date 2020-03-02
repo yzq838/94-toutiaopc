37b4aff72c86796e1f3c4b33e0bc9951e42d10a3
yzq838 <550279807@qq.com>
2020-03-02 22:47:28 +0800

    登录表单校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -6,22 +6,22 @@
         <div class="title">
           <img src="../../assets/img/logo_index.png" alt="">
         </div>
-        <!-- 表单 -->
-        <el-form>
-          <!-- 表单容器 -->
-          <el-form-item>
-            <!-- 表单域 -->
-            <el-input placeholder="请输入手机号"></el-input>
+        <!-- 表单 绑定model属性 绑定rules属性  ref手动校验方法-->
+        <el-form ref="loginForm" :model="loginForm" :rules="loginRules" style="margin-top:20px">
+          <!-- 表单容器 设置prop属性 prop表示要校验的字段名-->
+          <el-form-item prop="mobile">
+            <!-- 表单域 v-model双向绑定-->
+            <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
          </el-form-item>
-          <el-form-item>
-            <!-- 表单域  验证码-->
-            <el-input style="width:60%" placeholder="请输入验证码"></el-input>
+          <el-form-item prop="code" >
+            <!-- 表单域  验证码 prop校验字段-->
+            <el-input  v-model="loginForm.code" style="width:60%" placeholder="请输入验证码"></el-input>
             <!-- 防置按钮 -->
             <el-button style="float: right" plain>发送验证码</el-button>
           </el-form-item>
-          <el-form-item>
-            <!-- 表单域 勾选-->
-            <el-checkbox>我已阅读用户协议和隐私条款</el-checkbox>
+          <el-form-item  prop="checked"  >
+            <!-- 表单域 勾选 是否同意-->
+            <el-checkbox v-model="loginForm.checked">我已阅读用户协议和隐私条款</el-checkbox>
          </el-form-item>
          <el-form-item>
             <!-- 表单域 -->
@@ -34,7 +34,48 @@
 
 <script>
 export default {
+  data () {
+    return {
+      // 登录表单数据
+      loginForm: {
+        mobile: '', // 手机号
+        code: '', // 验证码
+        checked: false// 是否同意用户协议
+      },
+      // 表单验证规则
+      loginRules: {
+        // required为true表示该字段必填
+        mobile: [{ required: true, message: '您的手机号不能为空' }, {
+          pattern: /^1[3-9]\d{9}$/, // 正则表达式
+          message: '您的手机号格式不正确'
+        }],
+        code: [{ required: true, message: '您的验证码不能为空' }, {
+          pattern: /^\d{6}$/,
+          message: '验证码应该是6位数'
+        }],
+        // 自定义校验
+        checked: [{
+          validator: function (rule, value, callback) {
+            // rule是当前的校验规则
+            // value是当前的要校验的字段的值
+            // calllback是一个回调函数 不论成功或者失败都要执行
+            // 成功执行callback 失败执行 callback(new Error('错误信息'))
+            // 我们认为 如果 value 为true 就表示 校验成功 如果value 为false就表示校验失败
+            // new Error(错误信息) 就是我们提示的错误信息
+            value ? callback() : callback(new Error('您必须同意服务条款和隐私协议'))
+          }
+        }]
+      }
+    }
+  },
+  methods: {
+    login () {
+      //    this.$refs.loginForm 获取的就是el-form的对象实例
+      this.$refs.loginForm.validate().then(() => {
 
+      })
+    }
+  }
 }
 </script>
 
